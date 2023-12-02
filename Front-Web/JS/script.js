743f395ff484afdad59040d35dd5718132ceb0c4
xMasaki <masakinagahama2@hotmail.com>
2023-12-01 23:27:17 -0300

    Finalizando GS

--- Front-Web/JS/script.js
+++ Front-Web/JS/script.js
@@ -1,28 +1,39 @@
+const campoNome = document.querySelector("#nome");
 const campoEmail = document.querySelector("#email");
 const botaoValidacao = document.querySelector("#button2");
 
 const mensagem = document.createElement('m');
 
 function clearform() {
+    campoNome.value = '';
     campoEmail.value = '';
 }
 
 botaoValidacao.addEventListener("click", function(event) {
     event.preventDefault();
-    
+
+    if(campoNome.value.trim() === '') {
+        mensagem.textContent = "*Por favor preencha com seus dados!";
+        campoNome.insertAdjacentElement("afterend", mensagem);
+        mensagem.setAttribute("class", "error");
+        return false;
+    } 
     if(campoEmail.value.trim() === '') {
-        mensagem.textContent = "*Preencha o e-mail";
+        mensagem.textContent = "*Por favor preencha com seus dados!";
         campoEmail.insertAdjacentElement("afterend", mensagem);
         mensagem.setAttribute("class", "error");
         return false;
-    }
+    } 
 
+    const valorNome = campoNome.value;
     const valorEmail = campoEmail.value;
 
+    console.log(valorNome);
     console.log(valorEmail);
 
     mensagem.classList.add("d-none");
     clearform();
+    alert("Agradecemos o seu contato!");
 });
 
 var textos = ["Texto 1", "Texto 2", "Texto 3"];
